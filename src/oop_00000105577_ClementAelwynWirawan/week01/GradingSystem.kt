d088cb44efed47842813226bab74e56cfef116dd
MemenGanteng <clementwirawan2000@gmail.com>
2026-02-04 11:23:06 +0700

    week01: implement grading logic using when expression

--- src/oop_00000105577_ClementAelwynWirawan/week01/GradingSystem.kt
+++ src/oop_00000105577_ClementAelwynWirawan/week01/GradingSystem.kt
@@ -1,10 +1,18 @@
 package oop_00000105577_ClementAelwynWirawan.week01
 
 fun main() {
-    // REFACTOR: Gunakan val dan hapus tipe data eksplisit
     val name = "John Thor"
     val score = 80
 
-    // REFACTOR: String Template ($name)
     println("Nama: $name, Nilai: $score")
+
+    // Tambahkan di bawah println
+    val grade = when (score) {
+        in 90..100 -> "A"
+        in 80..89 -> "B"
+        in 70..79 -> "C"
+        else -> "D"
+    }
+
+    println("Grade kamu: $grade")
 }

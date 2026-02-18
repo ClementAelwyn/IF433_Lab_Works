c3bd3163848481104a85f93eb14a9e87e975c87a
MemenGanteng <clementwirawan2000@gmail.com>
2026-02-18 11:20:39 +0700

    week03: (task 1) implement Weapon testing in main

--- src/oop_00000105577_ClementAelwynWirawan/week03/Main.kt
+++ src/oop_00000105577_ClementAelwynWirawan/week03/Main.kt
@@ -2,15 +2,20 @@ package oop_00000105577_ClementAelwynWirawan.week03
 
 fun main() {
     val e = Employee("Budi")
-
-    e.salary = -1000
     e.salary = 5000000
     println("Gaji: ${e.salary}")
-
-
     e.increasePerformance()
-
-
-
+    e.printStatus()
     println("Pajak yang harus dibayar: ${e.tax}")
+
+    println("\n=== Test Weapon ===")
+    val sword = Weapon("Excalibur", 500)
+    println("Weapon: ${sword.name}, Damage: ${sword.damage}, Tier: ${sword.tier}")
+
+    sword.damage = -50
+    println("Damage setelah set -50: ${sword.damage}")
+
+    sword.damage = 9999
+    println("Damage setelah set 9999: ${sword.damage}")
+    println("Tier sekarang: ${sword.tier}")
 }

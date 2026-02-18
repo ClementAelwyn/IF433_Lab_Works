8968dd03d509246c2b487e77cd0c9254d7920578
MemenGanteng <clementwirawan2000@gmail.com>
2026-02-18 11:26:08 +0700

    week03: (task 2) implement Player testing in main

--- src/oop_00000105577_ClementAelwynWirawan/week03/Main.kt
+++ src/oop_00000105577_ClementAelwynWirawan/week03/Main.kt
@@ -7,15 +7,19 @@ fun main() {
     e.increasePerformance()
     e.printStatus()
     println("Pajak yang harus dibayar: ${e.tax}")
-
     println("\n=== Test Weapon ===")
     val sword = Weapon("Excalibur", 500)
     println("Weapon: ${sword.name}, Damage: ${sword.damage}, Tier: ${sword.tier}")
-
     sword.damage = -50
     println("Damage setelah set -50: ${sword.damage}")
-
     sword.damage = 9999
     println("Damage setelah set 9999: ${sword.damage}")
     println("Tier sekarang: ${sword.tier}")
+
+    println("\n=== Test Player ===")
+    val player = Player("Clement")
+
+    player.addXp(50)
+    player.addXp(60)
+
 }

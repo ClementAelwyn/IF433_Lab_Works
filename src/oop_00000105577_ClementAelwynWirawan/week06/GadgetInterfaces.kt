708a207397e5ca9250649007102d3a8700a58dce
MemenGanteng <clementwirawan2000@gmail.com>
2026-03-11 11:15:44 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000105577_ClementAelwynWirawan/week06/GadgetInterfaces.kt
@@ -0,0 +1,11 @@
+package oop_00000105577_ClementAelwynWirawan.week06
+
+class GadgetInterfaces {
+    interface Camera {
+        fun turnOn() { println("Lensa kamera terbuka dan sensor aktif.") }
+    }
+
+    interface Phone {
+        fun turnOn() { println("Sinyal seluler mencari jaringan.") }
+    }
+}

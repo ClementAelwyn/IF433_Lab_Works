b8ae2ddff85ca1c3e1099b8e13b3703a6be46bf0
MemenGanteng <clementwirawan2000@gmail.com>
2026-03-25 11:11:56 +0700

    week07: (task) create ItemRarity enum

---
+++ src/oop_00000105577_ClementAelwynWirawan/week07/GameModels.kt
@@ -0,0 +1,9 @@
+package oop_00000105577_ClementAelwynWirawan.week07
+
+enum class ItemRarity(val dropChance: Int) {
+    COMMON(70),
+    UNCOMMON(40),
+    RARE(15),
+    EPIC(5),
+    LEGENDARY(1)
+}

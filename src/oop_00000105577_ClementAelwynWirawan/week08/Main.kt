9a4ae439ab85b584e86028fd6d1e393ece3042b8
MemenGanteng <clementwirawan2000@gmail.com>
2026-04-15 10:43:54 +0700

    week08: implement safe call with let block

--- src/oop_00000105577_ClementAelwynWirawan/week08/Main.kt
+++ src/oop_00000105577_ClementAelwynWirawan/week08/Main.kt
@@ -10,4 +10,17 @@ fun main() {
     // yang kemudian ditangkap oleh Elvis operator
     val destination = emptyOrder.deliveryDetails?.address?.city?.name ?: "Kota Tidak Diketahui"
     println("Tujuan pengiriman: $destination")
+
+    println("\n=== TEST LET BLOCK ===")
+    val validOrder = Order(null, 250000)
+
+    // Idiom .let memastikan blok kode (dan variabel pembantunya)
+    // hanya dieksekusi jika objek aslinya tidak null
+    val receipt = validOrder.totalPrice?.let { price ->
+        // Blok ini HANYA jalan jika totalPrice tidak null
+        val tax = price * 0.11
+        "Transaksi Valid. Harga: Rp$price, Pajak: Rp$tax"
+    } ?: "Transaksi Invalid: Harga belum di-set!"
+
+    println(receipt)
 }
